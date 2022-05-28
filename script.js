0c7de1a7bf452625be45a5f1ea313905bf5a2f84
Ruchita Gosavi <ruchigosavi10@gmail.com>
2022-05-28 22:04:22 +0530

    Create book constructor & add book to localStorage whenever a book is added, create a new book instance from Book constructor and push it to the library array. Also, add the book to the localStorage.

--- script.js
+++ script.js
@@ -18,12 +18,30 @@ const clearFormInput = (formDiv) => {
     });
 }
 
+let library = [];
+
+// book constructor
+function Book(name, author, status) {   
+    this.name = name;
+    this.author = author;
+    this.status = status;
+}
+
 const addBook = (e) => {
     e.preventDefault();
     // remove the last child div bcoz it's a button
     const formDiv = [...e.target.children].slice(0, -1);
     const formDivInput = transformFormInput(formDiv);
     clearFormInput(formDiv);
+
+    // Create book instance/object
+    const book = new Book(formDivInput[0], formDivInput[1], formDivInput[2]);
+
+    // Add the book to the library array
+    library.push(book);
+
+    // Add book to localStorage
+    localStorage.setItem(`${book.name}`,JSON.stringify(book));
 }
 
 const addBookForm = document.querySelector("#add-book-form");
